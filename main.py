e0e4bbc7a58f6d2e65dddd98a1bf230d420744c2
dmitry <dshalayko@gmail.com>
2021-12-28 00:48:16 +0300

    работает загрузка видео и регистрация на сайте

--- main.py
+++ main.py
@@ -6,126 +6,53 @@ from telebot import custom_filters
 
 token = "5023550366:AAHHIsAIbrwI6S6ermjbeuZbvoa6Ye5qGwo"
 bot = telebot.TeleBot(token)
-token = {}
+
+
+def get_token(login, psw):
+    data = {
+        'telephone_number': login,
+        'password': psw,
+    }
+    post = requests.post('https://api.snaptrap.online/api/user/login', json=data)
+    token_tmp = jsonpickle.decode(post.text)['token']
+    auth = {
+        'Authorization': 'Bearer ' + token_tmp,
+    }
+    session = requests.get('https://api.snaptrap.online/api/user/auth', headers=auth)
+    token = jsonpickle.decode(session.text)['token']
+    return token
 
 
 class Product:
-    name = 'noname'
-    price = 0
-    type = 'none'
-    files = {}
+    files = 1
+    name = 2
+    price = 3
+    type = 4
 
 
-#
-# def get_token(login, psw):
-#     data = {
-#         'telephone_number': login,
-#         'password': psw,
-#     }
-#     post = requests.post('https://api.snaptrap.online/api/user/login', json=data)
-#     token_tmp = jsonpickle.decode(post.text)['token']
-#     auth = {
-#         'Authorization': 'Bearer ' + token_tmp,
-#     }
-#     session = requests.get('https://api.snaptrap.online/api/user/auth', headers=auth)
-#     token = jsonpickle.decode(session.text)['token']
-#     return token
-#
-#
-# @bot.message_handler(commands=['api'])
-# def start_message(message):
-#     response = requests.get('https://api.snaptrap.online/')
-#     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#     item1 = types.KeyboardButton("login")
-#     item2 = types.KeyboardButton("all users")
-#     markup.add(item1)
-#     markup.add(item2)
-#     bot.send_message(message.chat.id, response.text, reply_markup=markup)
-#
-#
-# @bot.message_handler(commands=['get_id'])
-# def button_message(message):
-#     bot.send_message(message.chat.id, message.chat.id)
-#
-#
-# @bot.message_handler(content_types=['text'])
-# def button_message(message):
-#     if message.text == "chat":
-#         bot.send_message(5031837131, 'test_chat')
-#
-#
-# @bot.message_handler(content_types='text')
-# def message_reply(message):
-#     if message.text == "login":
-#         new = get_token('123456789', 'qwertyQW_1')
-#         bot.send_message(message.chat.id, new)
-#     elif message.text == "all users":
-#         session = requests.get('http://localhost:8080/api/user/all', headers={
-#             'Authorization': 'Bearer ' + get_token('123456789', 'qwertyQW_1'),
-#         })
-#         bot.send_message(message.chat.id, session.text)
-
-
-# Загрузка товара(фото)
-# @bot.message_handler(content_types=['document'])
-# def handle_docs_photo(message):
-#     file_info = bot.get_file(message.document.file_id)
-#     downloaded_file = bot.download_file(file_info.file_path)
-#     src = 'D:\projects\\tg_bot_snaptrap\media\\' + message.document.file_name
-#
-#     files = {
-#         'img': open(src, 'rb')
-#     }
-#     bot.reply_to(message, "Enter product price")
-#
-#     @bot.message_handler(regexp=["\d"])
-#
-#     def message_product(message):
-#         price = message.text
-#         bot.reply_to(message, "Enter product name")
-#
-#         try:
-#             chat_id = message.chat.id
-#             file_info = bot.get_file(message.document.file_id)
-#             downloaded_file = bot.download_file(file_info.file_path)
-#             src = 'D:\projects\\tg_bot_snaptrap\media\\' + message.document.file_name
-#             with open(src, 'wb') as new_file:
-#                 new_file.write(downloaded_file)
-#
-#             data = {
-#                 'name': 'test-8654352',
-#                 'price': 434,
-#                 'type': 'img',
-#             }
-#             files = {
-#                 'img': open(src, 'rb')
-#             }
-#             post = requests.post('https://api.snaptrap.online/api/goods/', data=data, files=files, headers={
-#                 'Authorization': 'Bearer ' + get_token('123456789', 'qwertyQW_1'),
-#             })
-#
-#             bot.send_message(message.chat.id, post.text)
-#
-#             # bot.reply_to(message, "Пожалуй, я сохраню это")
-#         except Exception as e:
-#             bot.reply_to(message, e)
-
-@bot.message_handler(commands=['test'])
-def start_ex(message):
+@bot.message_handler(content_types=['document'])
+def handle_docs_photo(message):
     """
     Start command. Here we are starting state
     """
+    file_info = bot.get_file(message.document.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    src = 'D:\projects\\tg_bot_snaptrap\media\\' + message.document.file_name
+    with open(src, 'wb') as new_file:
+        new_file.write(downloaded_file)
+        if file_info.file_path.endswith('jpg'):
+            Product.files = {
+                'img': open(src, 'rb')
+            }
+            Product.type = 'img'
+        else:
+            Product.files = {
+                'img': open(src, 'rb')
+            }
+            Product.type = 'vid'
+
     bot.set_state(message.from_user.id, Product.name)
-    bot.send_message(message.chat.id, 'Product name')
-
-
-@bot.message_handler(state="*", commands='cancel')
-def any_state(message):
-    """
-    Cancel state
-    """
-    bot.send_message(message.chat.id, "Your state was cancelled.")
-    bot.delete_state(message.from_user.id)
+    bot.send_message(message.chat.id, 'Enter name')
 
 
 @bot.message_handler(state=Product.name)
@@ -133,31 +60,58 @@ def name_get(message):
     """
     State 1. Will process when user's state is 1.
     """
-    bot.send_message(message.chat.id, f'Now write price')
+    bot.send_message(message.chat.id, f'Price')
     bot.set_state(message.from_user.id, Product.price)
     with bot.retrieve_data(message.from_user.id) as data:
         data['name'] = message.text
 
 
-@bot.message_handler(state=Product.price)
-def ask_age(message):
-    """
-    State 2. Will process when user's state is 2.
-    """
-    bot.send_message(message.chat.id, "write type")
-    bot.set_state(message.from_user.id, Product.type)
-    with bot.retrieve_data(message.from_user.id) as data:
-        data['price'] = message.text
-
-
+# result
 @bot.message_handler(state=Product.price, is_digit=True)
 def ready_for_answer(message):
     with bot.retrieve_data(message.from_user.id) as data:
         bot.send_message(message.chat.id,
-                         "Ready, take a look:\n<b>Name: {name}\n Price: {price}\n</b>".format(name=data['name'],
-                                                                                              price=data['price']),
-                         parse_mode="html")
+                         "Ready, take a look:\n<b>Name: {name}\nPrice: {price}\n</b>".format(
+                             name=data['name'], price=message.text), parse_mode="html")
+
+        data_post = {
+            'name': data['name'],
+            'price': message.text,
+            'type': Product.type,
+        }
+        post = requests.post('https://api.snaptrap.online/api/goods/', data=data_post, files=Product.files, headers={
+            'Authorization': 'Bearer ' + get_token('123456789', 'qwertyQW_1'),
+        })
+        bot.send_message(message.chat.id, post.text)
     bot.delete_state(message.from_user.id)
 
 
-bot.infinity_polling()
+# incorrect number
+@bot.message_handler(state=Product.price, is_digit=False)
+def age_incorrect(message):
+    bot.send_message(message.chat.id, 'Looks like you are submitting a string in the field age. Please enter a number')
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Registration")
+    markup.add(item1)
+    bot.send_message(message.chat.id, "Welcome" + message.from_user.first_name, reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def button_message(message):
+    if message.text == "Registration":
+        bot.send_message(message.from_user.id, 'https://snaptrap.online?tgID=' +str(message.chat.id))
+
+
+# register filters
+
+bot.add_custom_filter(custom_filters.StateFilter(bot))
+bot.add_custom_filter(custom_filters.IsDigitFilter())
+
+# set saving states into file.
+bot.enable_saving_states()  # you can delete this if you do not need to save states
+
+bot.infinity_polling(skip_pending=True)
