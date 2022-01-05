a27385743d0de5db14c41d27c003d71de128869a
dmity <dshalayko@gmail.com>
2022-01-05 22:59:05 +0300

    fix dile location

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def handle_docs_photo(message):
     """
     file_info = bot.get_file(message.document.file_id)
     downloaded_file = bot.download_file(file_info.file_path)
-    src = 'media\\' + message.document.file_name
+    src = '\\media\\' + message.document.file_name
     with open(src, 'wb') as new_file:
         new_file.write(downloaded_file)
         if file_info.file_path.endswith('jpg') | file_info.file_path.endswith('png'):
